686e3b3806ff81bedecb5171ee016059a1396f9a
edoardodisaro <edoardo.disaro@mail.polimi.it>
2018-05-04 16:08:03 +0200

    Revert "Aggiunto metodo setValue a classe Die"
    This reverts commit 8f7f1670f7183e2ebeaf88c3c8b229c2bb41c010.

--- src/main/java/it/polimi/ingsw/Die.java
+++ src/main/java/it/polimi/ingsw/Die.java
@@ -37,10 +37,6 @@ public class Die {
             this.value = values[i];
         }
 
-        public void setValue (String value) {
-            this.value = value;
-        }
-
         public int getValueAsInt() {
             if(value.equals("1")) return 1;
             if(value.equals("2")) return 2;
