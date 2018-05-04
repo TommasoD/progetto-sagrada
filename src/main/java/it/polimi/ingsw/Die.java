02712fe1b56fc48f3b215ccad9af25e2a987c92d
davidedalcortivo <davide.dalcortivo@mail.polimi.it>
2018-05-04 16:18:13 +0200

    Aggiunti metodi per conversione in int della classe Die

--- src/main/java/it/polimi/ingsw/Die.java
+++ src/main/java/it/polimi/ingsw/Die.java
@@ -41,6 +41,25 @@ public class Die {
             this.value = value;
         }
 
+        public int getValueAsInt() {
+            if(value.equals("1")) return 1;
+            if(value.equals("2")) return 2;
+            if(value.equals("3")) return 3;
+            if(value.equals("4")) return 4;
+            if(value.equals("5")) return 5;
+            if(value.equals("6")) return 6;
+            return -1;
+        }
+
+        public int getColorAsInt() {
+            if(color.equals("RED")) return 0;
+            if(color.equals("GREEN")) return 1;
+            if(color.equals("BLUE")) return 2;
+            if(color.equals("YELLOW")) return 3;
+            if(color.equals("PURPLE")) return 4;
+            return -1;
+        }
+
         @Override
         public String toString() {
             String s = this.color;
