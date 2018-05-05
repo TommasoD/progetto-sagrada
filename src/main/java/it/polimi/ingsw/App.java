483db9e8534c7f19406b6672fc6c28bd3893aeba
TommasoD <tommaso.dolci@mail.polimi.it>
2018-05-05 18:19:46 +0200

    Aggiornata l'inizializzazione di tutte le finestre.

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,13 +1,11 @@
 package it.polimi.ingsw;
+import it.polimi.ingsw.windows.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        WindowPattern w = new Window2();
+        w.dump();
     }
 }
