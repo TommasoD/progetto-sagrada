04d2e26e5fb295eec999891c2395c45fb9d474f8
davidedalcortivo <davide.dalcortivo@mail.polimi.it>
2018-05-25 16:01:33 +0200

    Added ServerAcceptor, modified Server

--- src/main/java/it/polimi/ingsw/server/Server.java
+++ src/main/java/it/polimi/ingsw/server/Server.java
@@ -6,68 +6,17 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-import java.net.UnknownHostException;
-import java.net.InetAddress;
-
-public class Server {
-    private final int port = 7777;
-    private int i;
+public class Server extends Thread {
     protected ArrayList<ClientHandler> playerList = new ArrayList<ClientHandler>();
-    ServerSocket serverSocket;
 
-    public Server() {
-        try {
-            serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        System.out.println("Server ready");
-    }
+    public void run() {
 
-    public void startServer() {
+        System.out.println("Run...");
 
-        InetAddress ip;
-        try {
-            ip = InetAddress.getLocalHost();
-            System.out.println("Indirizzo ip: " + ip);
-        }
-        catch (UnknownHostException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-
-        //when the second player is connected the server must wait N seconds before starting the game
-        for (i = 0; i < 4; i++) {
-            try {
-                Socket socket = serverSocket.accept();
-                playerList.add(new ClientHandler(socket,this));
-                System.out.println("Client " + (i+1) + " connected");
-                playerList.get(i).start();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
-
-        while(i == 4) {
-            try {
-                Socket socket = serverSocket.accept();
-                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-                output.writeUTF("Too many players");
-                output.flush();
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        }
 
     }
 
-    public boolean nameUsed(String string) {
+    public synchronized boolean nameUsed(String string) {
         for (int i = 0; i < playerList.size()-1; i++) {
             String s = playerList.get(i).getUsername();
             if (s != null){
@@ -75,10 +24,5 @@ public class Server {
             }
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Server echoServer = new Server();
-        echoServer.startServer();
     }
 }
