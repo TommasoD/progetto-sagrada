d65925a07f234ea61f66d2e0f400585b8ba6e214
TommasoD <tommaso.dolci@mail.polimi.it>
2018-05-28 15:59:31 +0200

    Controller constructor changed.

--- src/main/java/it/polimi/ingsw/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/controller/Controller.java
@@ -16,8 +16,8 @@ public class Controller {
         constructor
      */
 
-    public Controller(){
-        this.model = new Game();
+    public Controller(Game model){
+        this.model = model;
         this.factory = new WindowPatternFactory();
     }
 
