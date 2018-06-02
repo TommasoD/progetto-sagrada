db915fce7522fd692dfbd3589d67a4c840ed0528
davidedalcortivo <davide.dalcortivo@mail.polimi.it>
2018-06-02 11:37:39 +0200

    modified CountdownTest

--- src/test/java/it/polimi/ingsw/server/CountdownTest.java
+++ src/test/java/it/polimi/ingsw/server/CountdownTest.java
@@ -6,27 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CountdownTest {
 
-    /**tested with 10 -> 10000, 20 -> 20000, 0 -> 20. */
     @Test
     void methods() {
 
         Countdown cd = new Countdown(new Server());
         cd.reset();
-        while(cd.read() < 10) {};
-        cd.stop();
-        assertEquals(10, cd.read());
+        while(cd.read() < 10000) {};
+        cd.stopClock();
+        assertEquals("" + 10, cd.toString());
         cd.resetAndStop();
-        assertEquals(0, cd.read());
-        cd.reset();
-        while(cd.read() < 10) {};
-        assertEquals(10, cd.read());
-        cd.stop();
-        cd.resume();
-        assertEquals(10, cd.read());
-        while(cd.read() < 20) {};
-        cd.stop();
-        assertEquals(20, cd.read());
         assertEquals("" + 0, cd.toString());
+        cd.reset();
+        while(cd.read() < 10000) {};
+        assertEquals("" + 10, cd.toString());
+        cd.stopClock();
+        cd.resumeClock();
+        assertEquals("" + 10, cd.toString());
+        while(cd.read() < 20000) {};
+        cd.stopClock();
+        assertEquals("" + 20, cd.toString());
 
     }
 }
