8adefebac7bd6e2359c93d558aaf1a2c1041a0fd
TommasoD <tommaso.dolci@mail.polimi.it>
2018-06-05 15:06:21 +0200

    New class to validate tool card actions.

---
+++ src/main/java/it/polimi/ingsw/controller/ToolCardCheck.java
@@ -0,0 +1,55 @@
+package it.polimi.ingsw.controller;
+
+import it.polimi.ingsw.model.Die;
+import it.polimi.ingsw.model.WindowPattern;
+
+public class ToolCardCheck {
+
+    public boolean toolCard1(Die d, int action){
+
+
+        return false;
+    }
+
+    public boolean toolCard2(WindowPattern window, int x, int y, int a, int b){
+
+
+        return false;
+    }
+
+    public boolean toolCard3(WindowPattern window, int x, int y, int a, int b){
+
+
+        return false;
+    }
+
+    public boolean toolCard4(WindowPattern window, int x, int y, int a, int b, int x2, int y2, int a2, int b2){
+
+
+        return false;
+    }
+
+    public boolean toolCard6(WindowPattern window, Die d, int x, int y){
+
+
+        return false;
+    }
+
+    public boolean toolCard8(WindowPattern window, Die d, int x, int y){
+
+
+        return false;
+    }
+
+    public boolean toolCard9(WindowPattern window, Die d, int x, int y){
+
+
+        return false;
+    }
+
+    public boolean toolCard12(WindowPattern window, int x, int y, int a, int b, int x2, int y2, int a2, int b2){
+
+
+        return false;
+    }
+}
