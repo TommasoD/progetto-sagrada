352e5f6effd249347f482c77f85cdee08422717b
davidedalcortivo <davide.dalcortivo@mail.polimi.it>
2018-06-08 13:51:04 +0200

    modified class ToolCardCheck

--- src/main/java/it/polimi/ingsw/controller/ToolCardCheck.java
+++ src/main/java/it/polimi/ingsw/controller/ToolCardCheck.java
@@ -15,22 +15,39 @@ public class ToolCardCheck {
 
     public boolean toolCard2(WindowPattern window, int x, int y, int a, int b){
         if (window.getWindowMatrix(x, y).isNotEmpty()) {
-            if (window.isValidWithoutColors(a, b, window.getWindowMatrix(x, y).getDie())) return true;
+            Die die = window.getWindowMatrix(x, y).removeDie();
+            if (window.isValidWithoutColors(a, b, die)) {
+                window.getWindowMatrix(x, y).setDie(die);
+                return true;
+            }
+            window.getWindowMatrix(x, y).setDie(die);
         }
         return false;
     }
 
     public boolean toolCard3(WindowPattern window, int x, int y, int a, int b){
         if (window.getWindowMatrix(x, y).isNotEmpty()) {
-            if (window.isValidWithoutValues(a, b, window.getWindowMatrix(x, y).getDie())) return true;
+            Die die = window.getWindowMatrix(x, y).removeDie();
+            if (window.isValidWithoutValues(a, b, die)) {
+                window.getWindowMatrix(x, y).setDie(die);
+                return true;
+            }
+            window.getWindowMatrix(x, y).setDie(die);
         }
         return false;
     }
 
     public boolean toolCard4(WindowPattern window, int x, int y, int a, int b, int x2, int y2, int a2, int b2){
         if (window.getWindowMatrix(x, y).isNotEmpty() && window.getWindowMatrix(x2, y2).isNotEmpty() && (x != x2 || y != y2) && (a != a2 || b != b2)) {
-            if (window.isValid(a, b, window.getWindowMatrix(x, y).getDie()) && window.isValid(a2, b2, window.getWindowMatrix(x2, y2).getDie()))
+            Die die1 = window.getWindowMatrix(x, y).removeDie();
+            Die die2 = window.getWindowMatrix(x2, y2).removeDie();
+            if (window.isValid(a, b, die1) && window.isValid(a2, b2, die2)) {
+                window.getWindowMatrix(x, y).setDie(die1);
+                window.getWindowMatrix(x2, y2).setDie(die2);
                 return true;
+                }
+            window.getWindowMatrix(x, y).setDie(die1);
+            window.getWindowMatrix(x2, y2).setDie(die2);
             }
         return false;
     }
@@ -54,11 +71,19 @@ public class ToolCardCheck {
 
     public boolean toolCard12(ArrayList<Die> track, WindowPattern window, int x, int y, int a, int b, int x2, int y2, int a2, int b2){
         if (window.getWindowMatrix(x, y).isNotEmpty() && window.getWindowMatrix(x2, y2).isNotEmpty() && (x != x2 || y != y2) && (a != a2 || b != b2)) {
-            if (window.isValid(a, b, window.getWindowMatrix(x, y).getDie()) && window.isValid(a2, b2, window.getWindowMatrix(x2, y2).getDie()) && window.getWindowMatrix(x, y).getDie().getColor().equals(window.getWindowMatrix(x2, y2).getDie().getColor())) {
+            Die die1 = window.getWindowMatrix(x, y).removeDie();
+            Die die2 = window.getWindowMatrix(x2, y2).removeDie();
+            if (window.isValid(a, b, die1) && window.isValid(a2, b2, die2) && die1.getColor().equals(die2.getColor())) {
                 for(int i = 0; i < track.size(); i++) {
-                    if(window.getWindowMatrix(x, y).getDie().getColor().equals(track.get(i).getColor())) return true;
+                    if(die1.getColor().equals(track.get(i).getColor())) {
+                        window.getWindowMatrix(x, y).setDie(die1);
+                        window.getWindowMatrix(x2, y2).setDie(die2);
+                        return true;
+                    }
                 }
             }
+            window.getWindowMatrix(x, y).setDie(die1);
+            window.getWindowMatrix(x2, y2).setDie(die2);
         }
         return false;
     }
