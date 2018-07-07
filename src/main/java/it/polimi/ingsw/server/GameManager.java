6e24cb4ac81697aacb02fd1f0c7b9b212dfaf313
TommasoD <tommaso.dolci@mail.polimi.it>
2018-07-07 10:17:17 +0200

    Javadoc updated in GameManager.

--- src/main/java/it/polimi/ingsw/server/GameManager.java
+++ src/main/java/it/polimi/ingsw/server/GameManager.java
@@ -5,10 +5,14 @@ import it.polimi.ingsw.utils.Observer;
 import java.util.ArrayList;
 
 /**
- *
+ * Contains a reference to all the client connections; when someone sends a message
+ * to the server side, the class notifies the controller. Moreover, whenever the model
+ * changes and a message is created as an answer, it forwards the message to the correct
+ * connection thread, so that it can be sent across the network to the right client.
  */
 
 public class GameManager extends Observable<String> implements Observer<String> {
+
     protected ArrayList<ClientHandler> playerList;
 
     /**
@@ -20,7 +24,7 @@ public class GameManager extends Observable<String> implements Observer<String> 
     }
 
     /**
-     *
+     * Sends a message to all the players in the game, when notified by the model.
      * @param message the message depicting the event.
      */
 
@@ -31,7 +35,7 @@ public class GameManager extends Observable<String> implements Observer<String> 
     }
 
     /**
-     *
+     * Sends a message to a player, when notified by the model.
      * @param message the message depicting the event.
      * @param id the id of the player associated with the event.
      */
@@ -43,9 +47,9 @@ public class GameManager extends Observable<String> implements Observer<String> 
     }
 
     /**
-     *
-     * @param message
-     * @param player
+     * Notifies the controller of a certain event.
+     * @param message the message depicting the event.
+     * @param player the id of the player associated with the event.
      */
 
     public void notifyController(String message, int player) {
